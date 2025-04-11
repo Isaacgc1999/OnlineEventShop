3a9134f5624ea6dbb571f8203100160f83b92ea5
Isaac <isaactraba@gmail.com>
2025-04-11 17:19:34 +0200

    basic project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CatalogueComponent } from './features/catalogue/catalogue.component';
+import { EventInfoComponent } from './features/event-info/event-info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: CatalogueComponent,
+        children: [
+        { path: '', component: CatalogueComponent },
+        { path: 'event/:id', component: EventInfoComponent }
+        ]
+    },
+    { path: '**', redirectTo: '' }
+];
---
+++ src/app/features/event-info/event-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-info',
+  imports: [],
+  templateUrl: './event-info.component.html',
+  styleUrl: './event-info.component.scss'
+})
+export class EventInfoComponent {
+
+}
