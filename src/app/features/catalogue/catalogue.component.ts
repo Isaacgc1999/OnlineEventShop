fb5c27be92277570af547f9f63d093e1de1baf31
Isaac <isaactraba@gmail.com>
2025-04-11 20:49:07 +0200

    event distribution inside catalogue + disposition in smaller screens

--- src/app/features/catalogue/catalogue.component.ts
+++ src/app/features/catalogue/catalogue.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CardComponent } from "../../shared/components/card/card.component";
+import { CatalogueService } from '../../core/services/catalogue.service';
+import { Event } from '../../core/models/event.model';
 
 @Component({
   selector: 'app-catalogue',
@@ -8,6 +10,13 @@ import { CardComponent } from "../../shared/components/card/card.component";
   templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.scss'
 })
-export class CatalogueComponent {
+export class CatalogueComponent  implements OnInit{
+  catalogueService = inject(CatalogueService);
+  events: Event[] = [];
 
+  ngOnInit(){
+    this.catalogueService.getEvents().subscribe((events) => {
+      this.events = events;
+    });
+  }
 }
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input, output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ButtonComponent } from "../button/button.component";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule, ButtonComponent],
+  imports: [MatCardModule, ButtonComponent, DatePipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
