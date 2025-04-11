15f690eca5721160700877451774a63ce9614edd
Isaac <isaactraba@gmail.com>
2025-04-11 18:19:56 +0200

    header section

--- src/app/core/models/event.model.ts
+++ src/app/core/models/event.model.ts
@@ -2,9 +2,9 @@ export interface Event {
     id: string;
     title: string;
     subtitle: string;
+    description: string;
     image: string;
     place: string;
     startDate: string;
     endDate: string;
-    description: string;
 }
