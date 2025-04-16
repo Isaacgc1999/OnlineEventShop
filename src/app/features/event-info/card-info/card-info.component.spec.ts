c07b1968943b32117aa8086cf3e48fd99ae0084b
Isaac <isaactraba@gmail.com>
2025-04-16 18:40:15 +0200

    basic test configuration + located colors.scss to be able to compile tests

--- src/app/features/event-info/card-info/card-info.component.spec.ts
+++ src/app/features/event-info/card-info/card-info.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CardInfoComponent } from './card-info.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CardInfoComponent', () => {
   let component: CardInfoComponent;
@@ -8,7 +9,7 @@ describe('CardInfoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CardInfoComponent]
+      imports: [CardInfoComponent, HttpClientTestingModule],
     })
     .compileComponents();
 
--- src/app/features/event-info/event-info.component.spec.ts
+++ src/app/features/event-info/event-info.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { EventInfoComponent } from './event-info.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('EventInfoComponent', () => {
   let component: EventInfoComponent;
@@ -8,7 +9,7 @@ describe('EventInfoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EventInfoComponent]
+      imports: [EventInfoComponent, HttpClientTestingModule, RouterTestingModule ]
     })
     .compileComponents();
 
